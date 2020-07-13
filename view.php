fa023e6be7527e37323a0c00445976e3c0c55ae7
Simranjeet1 <67561103+Simranjeet1@users.noreply.github.com>
2020-07-13 01:51:08 -0400

    Update view.php

--- view.php
+++ view.php
@@ -1,49 +1,22 @@
-<?php require_once('header.php'); ?>
-<body class="view">
-<div class="container inner">
-<header class="masthead mb-auto">
-    <div class="inner">
-      <h3 class="masthead-brand">TuneShare</h3>
-      <nav class="nav nav-masthead justify-content-center">
-        <a class="nav-link" href="index.php">Home</a>
-        <a class="nav-link" href="add.php">Share Your Tune</a>
-        <a class="nav-link" href="view.php">View Playlists</a>
-      </nav>
-    </div>
-  </header>
+<?php session_start(); ?>
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title> Take A Look </title>
+</head>
+
+<body>
+    <h3> Here's a Cat! </h3>
     <?php
-    try {
-    //connect to our db 
-    require_once('connect.php'); 
-
-    //set up SQL statement 
-    $sql = "SELECT * FROM songs;"; 
-
-    //prepare the query 
-    $statement = $db->prepare($sql);
-
-    //execute 
-    $statement->execute(); 
-
-    //use fetchAll to store the results 
-    $records = $statement->fetchAll(); 
-
-    // echo out the top of the table 
-
-    echo "<table class='table'>";
-
-    foreach ($records as $record) {
-        echo "<tr><td><img src='images/". $record['photo']. "' alt='" . $record['photo'] . "'></td><td>"
-        . $record['first_name'] . "</td><td>" . $record['last_name'] . "</td><td>" . $record['genre'] . "</td><td>" . $record['location'] . "</td><td>" . $record['email'] . "</td><td>" . $record['favsong']. "</td><td><a href='" . $record['link']. "' target='_blank'> Listen Now </a></td><td><a href='delete.php?id=" . $record['user_id'] . "'> Delete </a></td><td><a href='add.php?id=" . $record['user_id'] . "'>Edit </a></td></tr>";
-        }
-     echo "</tbody></table>"; 
-
-     $statement->closeCursor(); 
-    }
-    catch(PDOException $e) {
-        $error_message = $e->getMessage(); 
-        echo "<p> $error message </p>"; 
+    if (isset($_SESSION['name'])) {
+        echo "<p> Hey There " . $_SESSION['name'] . "!";
+    } else {
+        echo "<p> Hey There! ";
     }
     ?>
-    </main>
-    <?php require_once('footer.php'); ?>
+    <a href="destroy.php"> Forget Me ! </a>
+</body>
+
+</html>
